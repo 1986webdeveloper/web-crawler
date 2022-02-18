6842d75f158edced076f56c68883c7a2bb5efb72
bgbackenddev <python.developer@1tab.onmicrosoft.com>
2022-02-18 19:06:53 +0530

    update code and designe~

--- page_speed_insights/views.py
+++ page_speed_insights/views.py
@@ -9,14 +9,14 @@ from .models import PageSeedInsight
 # Create your views here.
 
 
-def report_data(request):
+def report_data():
     queryset = PageSeedInsight.objects.filter(domain='http://acquaintsoft.com')
     context={}
     context['data'] = queryset
-    return render(request, "pagespeed/report.html", context)
+    return render("pagespeed/report.html", context)
 
 
-def page_speed_scrap_url(request, domain_obj):
+def page_speed_scrap_url(domain_obj):
     urls = DomainUrl.objects.filter(domain__name=domain_obj.name)
     for i in urls:
         google_speed_page_url = os.environ['PAGESPEED_URLS'] + '?url=' + i.url + '&key=' + os.environ['PAGESPEED_KEY']
@@ -26,8 +26,8 @@ def page_speed_scrap_url(request, domain_obj):
     return redirect("page_speed_insights:report")
 
 
-def detail_report_data(request, pk):
+def detail_report_data(pk):
     queryset = PageSeedInsight.objects.filter(id=pk)
     context = {}
     context['data'] = queryset
-    return render(request, "pagespeed/report_data.html", context)
+    return render("pagespeed/report_data.html", context)
