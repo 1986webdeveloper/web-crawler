2bc412a377ca66ded094592e8786d0a640d27114
pcbackend <python.dev3@gmail.com>
2022-03-01 16:06:34 +0530

    flag data fixes

--- page_speed_insights/views.py
+++ page_speed_insights/views.py
@@ -5,6 +5,8 @@ import json
 import os
 import urllib.request
 import logging
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -57,19 +59,16 @@ def page_speed_scrap_url(obj):
     send_report_status(email)
 
 
-def flag_data(request):
+def flag_data(request, pk):
     """
            Flag Data for cron
     """
     data = request.POST["flag"]
-    queryset = PageSeedInsight.objects.all()
-    for i in queryset:
-        i.cron_flag = data
-        i.save()
-    return render(request, "pagespeed/report.html")
+    PageSeedInsight.objects.filter(domain_fk=pk).update(cron_flag = data)
+    return JsonResponse({"message": "Status updated successfully!!"})
 
 
-class PermissionAjaxDatatableView(AjaxDatatableView):
+class PageInsightAjaxDatatableView(AjaxDatatableView):
 
     model = PageSeedInsight
     title = 'Page Insight Data'
@@ -77,12 +76,15 @@ class PermissionAjaxDatatableView(AjaxDatatableView):
     length_menu = [[10, 20, 50, 100, -1], [10, 20, 50, 100, 'all']]
     search_values_separator = '+'
 
+
     column_defs = [
-        AjaxDatatableView.render_row_tools_column_def(),
         {'name': 'id', 'visible': False, },
         {'name': 'url', 'visible': True, },
-        {'name': 'action', 'visible': True, },
+        {'name': 'action', 'visible': True, 'searchable': False},
     ]
+
+    def render_row_details(self, pk, request=None):
+        return
 
     def get_initial_queryset(self, request=None):
         domain_id = self.request.resolver_match.kwargs.get('pk')
